bb616a0fed91b1ccf65ef15bd987b8901b49cee2
EC2 Default User <ec2-user@ip-172-31-25-139.us-east-2.compute.internal>
2019-04-05 14:56:27 +0000

    comments

--- auto-ci.sh
+++ auto-ci.sh
@@ -1,24 +1,75 @@
 #!/bin/bash
+# change directory to /home/ec2-user (user home)
 cd ~
+
+# install Git using Yellowdog Update Manager
 sudo yum install -y git
+
+# check install succeeded
 git --version
+
+# use HTTP GET to fetch maven
 wget "http://mirrors.gigenet.com/apache/maven/maven-3/3.6.0/binaries/apache-maven-3.6.0-bin.tar.gz"
+
+# list contents of current directory
 ls
+
+# print working directory (where you are now)
 pwd
-tar -zxvf apache-maven-3.6.0-bin.tar.gz
-mv apache-maven-3.6.0 maven
+
+# expands a gunzip file (tar means it is a folder and not a single file only)
+tar -zxf ~/apache-maven-3.6.0-bin.tar.gz
+
+# remove unneeded file
+rm ~/apache-maven-3.6.0-bin.tar.gz
+
+# rename files by moving them
+mv ~/apache-maven-3.6.0 ~/maven
+
+# use HTTP GET to fetch Tomcat
 wget "http://mirror.reverse.net/pub/apache/tomcat/tomcat-8/v8.5.39/bin/apache-tomcat-8.5.39.tar.gz"
-tar -zxvf apache-tomcat-8.5.39.tar.gz
-mv apache-tomcat-8.5.39 tomcat
+
+# expands gunzip file
+tar -zxf ~/apache-tomcat-8.5.39.tar.gz
+
+# remove unneeded file
+rm ~/apache-tomcat-8.5.39.tar.gz 
+
+# rename files by moving them
+mv ~/apache-tomcat-8.5.39 ~/tomcat
+
+# add manager-gui role and tomcat user with this role.. 
+# echo prints something and > pipes the content into the file 
+# ( > overwrites while >> will concatenate)
 echo "<?xml version='1.0' encoding='utf-8'?><tomcat-users><role rolename=\"manager-gui\"/><role rolename=\"manager-script\"/><user username=\"tomcat\" password=\"tomcat\" roles=\"manager-gui\"/><user username=\"maven\" password=\"maven\" roles=\"manager-script\"/></tomcat-users>" > tomcat/conf/tomcat-users.xml
+
+# allow the remote access of Tomcat manager console
+# removes the default RemoteAddrValve constraint
 echo "<?xml version='1.0' encoding='UTF-8'?><Context antiResourceLocking=\"false\" privileged=\"true\"><Manager sessionAttributeValueClassNameFilter=\"java\.lang\.(?:Boolean|Integer|Long|String)|org\.apache\.catalina\.filters\.CsrfPreventionFilter\$LruCache(?:\$1)?|java\.util\.(?:Linked)>HashMap\"/></Context>" > tomcat/webapps/manager/META-INF/context.xml
+
+# setup maven tomcat plugin credentials
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?><settings xmlns=\"http://maven.apache.org/SETTINGS/1.0.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://maven.apache.org/SETTINGS/1.0.0 http://maven.apache.org/xsd/settings-1.0.0.xsd\"><localRepository>/home/ec2-user/.m2/repository</localRepository><servers><server><id>maven</id><username>maven</username><password>maven</password></server></servers></settings>" > maven/conf/settings.xml
+
+# install OpenJDK 8
 sudo yum install -y java-1.8.0-openjdk-devel.x86_64
+
+# creates an environment variable for this Shell session. 
+# long-term variables can go into /etc/environment file
 export JAVA_HOME=/usr/lib/jvm/java-openjdk
+
+# print an environment variable with $variable_name
 echo $JAVA_HOME
+
+# use HTTP GET to fetch the latest Jenkins war
 wget http://mirrors.jenkins.io/war/latest/jenkins.war
-mv jenkins.war tomcat/webapps/jenkins.war
-sudo sh tomcat/bin/startup.sh
-sudo cat /root/.jenkins/secrets/initialAdminPassword
 
+# moving a war into webapps folder will deploy the war
+mv ~/jenkins.war ~/tomcat/webapps/jenkins.war
 
+# executes a shell command to start Tomcat
+sudo sh ~/tomcat/bin/startup.sh
+
+# prints the Jenkins admin credentials
+# this only works after browsing to your.aws.ip:8080/jenkins
+# hence it is commented here
+# sudo cat /root/.jenkins/secrets/initialAdminPassword
